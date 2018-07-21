f062c054cb2008325bd35ecbf95d95dfb07f79c0
NatashaSenah <tashsenah@gmail.com>
2018-07-21 12:58:09 +0300

    Add the scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-
+function myFunction(){
+  var x= document.getElementById("demo")
+  x.innerHTML =Math.floor((Math.random() *6) +1);
+}
+function myFunctions(){
+  var s= document.getElementById("speak")
+  s.innerHTML =Math.floor((Math.random() *100) +1);
+}
